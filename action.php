7b2d1d1cb12331d1fe89f5abcd21bc505126c613
lancylord-dmonte <80383614+lancylord-dmonte@users.noreply.github.com>
2021-03-12 13:41:12 +0530

    Update action.php

--- action.php
+++ action.php
@@ -1,14 +1,16 @@
-<html><body>
 
 <?php
- if($isset($_POST['submit']))
- {$to = "lancydmonte@gmail.com";
+
+ $to = "lancydmonte@gmail.com";
 $sub = $_POST['subject'];
 $txt =$_POST['message'];
 $headers = $_POST['email'];
- }
+
 
 echo "done";
- echo $to+$sub+$txt;
+ echo $sub;
+
+
+exit();
 ?>
-  </body></html>
+ 
